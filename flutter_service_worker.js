d2ff610cc4e0c6a908822e836b99cf8b4b41c7b8
MeloTsui <qonkgonk@gmail.com>
2022-03-25 22:40:52 +0800

    menu ui, loop meatlist

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1633f62f084b992023fa4cc652db3730",
-"/": "1633f62f084b992023fa4cc652db3730",
-"main.dart.js": "954b73285ffd6f93a6db2d47d9268763",
+"index.html": "e667369ffc0f812b86b51d8118436472",
+"/": "e667369ffc0f812b86b51d8118436472",
+"main.dart.js": "6c7e7547c52a9d7cd642bfd1a98ea54a",
 "manifest.json": "484bcfaee7dda26c8934b6f0a1f7c1c1",
 "version.json": "b3c25d78bb96f4352f7edc20a377d58b"
 };
