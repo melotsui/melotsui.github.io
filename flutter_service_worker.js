de6db89832f1eb64423391f307e1a080f61cf42e
MeloTsui <qonkgonk@gmail.com>
2022-03-25 23:54:38 +0800

    update menu bottom bar

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e667369ffc0f812b86b51d8118436472",
-"/": "e667369ffc0f812b86b51d8118436472",
-"main.dart.js": "6c7e7547c52a9d7cd642bfd1a98ea54a",
+"index.html": "a6d0be3d83334558131a073ac5891104",
+"/": "a6d0be3d83334558131a073ac5891104",
+"main.dart.js": "5ba78bd33f474d85a945ecff5a55bd5d",
 "manifest.json": "484bcfaee7dda26c8934b6f0a1f7c1c1",
 "version.json": "b3c25d78bb96f4352f7edc20a377d58b"
 };
